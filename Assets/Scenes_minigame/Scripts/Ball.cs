13cad38cae7ccb2764a5af1135fb1514538e28ff
Biter29 <31832941+Biter29@users.noreply.github.com>
2018-09-09 17:59:09 -0400

    Update video 2
    Features added player movement, by using a C# script

---
+++ Assets/Scenes_minigame/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate() {
+        float movHorizontal = Input.GetAxis("Horizontal");
+        float movVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(movHorizontal, 0.0f, movVertical);
+
+        rb.AddForce(movement*speed);
+     }
+}
