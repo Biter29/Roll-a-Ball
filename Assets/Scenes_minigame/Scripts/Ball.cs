f3ed730ceef20f75d1a732d4abc242139efcb73d
Biter29 <31832941+Biter29@users.noreply.github.com>
2018-09-12 04:49:02 -0400

    Collectable count and you win
    This is the finished game in that the player can now win the game and a count is displayed.

--- Assets/Scenes_minigame/Scripts/Ball.cs
+++ Assets/Scenes_minigame/Scripts/Ball.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void FixedUpdate() {
         float movHorizontal = Input.GetAxis("Horizontal");
@@ -25,6 +32,16 @@ public class Ball : MonoBehaviour {
         if (other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
